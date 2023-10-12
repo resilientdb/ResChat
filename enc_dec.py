98e5998b6ff65ffbbd95661a9bdd87dd2df17774
NoBugInMyCode <knysun@ucdavis.edu>
2023-10-11 19:06:38 -0700

    gpg

--- enc_dec.py
+++ enc_dec.py
@@ -1,24 +1,41 @@
-from getmac import get_mac_address as gma
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Protocol.KDF import PBKDF2
+import gnupg, os
 
-def PBKDF2_encrypt(mac: str) -> bytes:
-    """Use PBKDF2 to encrypt MAC address"""
-    return PBKDF2(mac, bytes(16), dkLen=32, count=1000000)
 
-def aes_encrypt(data: str, key: bytes) -> tuple:
-    """Encrypts a string using AES encryption."""
-    cipher = AES.new(key, AES.MODE_CBC)
-    ct_bytes = cipher.encrypt(pad(data.encode(), AES.block_size))
-    return cipher.iv.hex(), ct_bytes.hex()  # Convert bytes to hex strings
+class Local_client:
+    def __init__(self):
+        self.gpg = gnupg.GPG()
 
-def aes_decrypt(iv: str, ct: str, key: bytes) -> str:
-    """Decrypts an AES encrypted message."""
-    cipher = AES.new(key, AES.MODE_CBC, bytes.fromhex(iv))
-    pt = unpad(cipher.decrypt(bytes.fromhex(ct)), AES.block_size)
-    return pt.decode()
-def get_mac():
-    return gma()
+    def create_user(self, user_name, email, password):
+        alice = {'name_real': user_name,
+                 'name_email': email,
+                 'expire_date': '2029-04-01',
+                 'key_type': 'RSA',
+                 'key_length': 1024,
+                 'key_usage': '',
+                 'subkey_type': 'RSA',
+                 'subkey_length': 1024,
+                 'subkey_usage': 'encrypt,sign,auth',
+                 'passphrase': password}
+        input_data = self.gpg.gen_key_input(**alice)
+        key = self.gpg.gen_key(input_data)
+        public_key = self.gpg.export_keys(keyids=email, secret=False)
+        private_key = self.gpg.export_keys(keyids=email, secret=True, passphrase=password)
+        with open("public_key.txt", 'w') as file:
+             file.write(public_key)
+        with open("private_key.txt", 'w') as file:
+             file.write(private_key)
+        return public_key, private_key
 
+
+if __name__ == '__main__':
+    l = Local_client()
+    public, private = l.create_user('kenny', 'kny@gmail.com', '123')
+
+    plaintext = "This is a secret message"
+    encrypted_data = l.gpg.encrypt(plaintext, 'kny@gmail.com')
+    encrypted_string = str(encrypted_data)
+    print(f"Encrypted string: {encrypted_string}")
+
+    decrypted_data = l.gpg.decrypt(message=encrypted_string)
+    print(decrypted_data)
 
