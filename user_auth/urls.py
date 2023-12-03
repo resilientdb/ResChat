31425b50043875d65bb9063a6f667c5203a25f3f
Britney Nguyen <nguyen26.bn@gmail.com>
2023-12-02 19:09:08 -0800

    Clean up

--- user_auth/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.key_auth_page, name='key_auth_page'),
-]
--- user_auth/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def key_auth_page(request):
-    return render(request, 'user_auth/key_auth_page.html')
