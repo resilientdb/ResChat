8e557d37413494280c57e6a229f971c5a0f1a3f1
NoBugInMyCode <knysun@ucdavis.edu>
2024-01-22 18:58:45 -0800

    update kv_operation

--- kv_operation.py
+++ kv_operation.py
@@ -1,70 +1,32 @@
-import subprocess
-from page import Page
 import os
+import sys
+
+config_path = None
+
+sys.path.append("bazel-out/k8-fastbuild/bin/")
+import pybind_kv
 
 
-# Working directory where the bazel workspace is located
-# working_directory = os.path.expanduser('~/Desktop/incubator-resilientdb')
-# Config path relative to the working_directory
-# config_path = "/home/ubuntu/Desktop/incubator-resilientdb/scripts/deploy/config_out/client.config"
-
-# config_path = ""
-
-
-def assign_resdb_path(master_folder_location: str) -> bool:
-    config_path = os.path.join(master_folder_location, "scripts/deploy/config_out/client.config")
-    if os.path.exists(config_path):
-        with open(f"resdb_path.config", "w") as f:
-            f.write(master_folder_location)
-            return True
-    else:
-        print(f"{master_folder_location}/scripts/deploy/config_out/client.config does not exists.")
-        return False
-
-
-def load_resdb_path() -> bool or str:
-    if os.path.exists("resdb_path.config"):
-        with open(f"resdb_path.config", "r") as f:
-            config_path = f.readline()
-            if os.path.exists(config_path):
-                return config_path
-            else:
-                print(f"{config_path} does not exists.")
-                return False
-    else:
-        print("Please assign ResilientDB path first")
-        return False
+def load_config_path() -> bool:
+    global config_path
+    if os.path.exists("config_path.config"):
+        with open("config_path.config", "r") as f:
+            config_path = f.readline().strip()
+            return os.path.exists(config_path)
+    return False
 
 
 def send_message(key: str, value: str):
-    command = [
-        "bazel", "run", "//service/tools/kv/api_tools:kv_service_tools", "--",
-        config_path, "set", key, value
-    ]
-
-    result = subprocess.run(command, capture_output=True, text=True, cwd=working_directory)
-    if result.returncode != 0:
-        print("Error executing command:", result.stderr)
-        return False
-    if "client set ret = 0" in result.stdout:
-        return True
-    else:
-        print("Unexpected output:", result.stdout)
-        return False
+    global config_path
+    if config_path is None:
+        if not load_config_path():
+            raise ValueError("无法加载配置文件路径")
+    pybind_kv.set(key, value, config_path)
 
 
-def parse_get_stdout(output):
-    stripped_output = output[len('client get value = '):]
-    return stripped_output
-
-
-def get_message(key: str):
-    command = [
-        "bazel", "run", "//service/tools/kv/api_tools:kv_service_tools", "--",
-        config_path, "get", key
-    ]
-    result = subprocess.run(command, capture_output=True, text=True, cwd=working_directory)
-    if result.returncode != 0:
-        print("Error executing command:", result.stderr)
-        return ""
-    return parse_get_stdout(result.stdout)
+def get_message(key: str) -> str:
+    global config_path
+    if config_path is None:
+        if not load_config_path():
+            raise ValueError("无法加载配置文件路径")
+    return pybind_kv.get(key, config_path)
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import sys
-sys.path.append("bazel-out/k8-fastbuild/bin/")
-import pybind_kv
+import kv_operation as kv
 
-pybind_kv.set("test", "123", "/home/ubuntu/Desktop/incubator-resilientdb/scripts/deploy/config_out/client.config")
-print(pybind_kv.get("test", "/home/ubuntu/Desktop/incubator-resilientdb/scripts/deploy/config_out/client.config"))
+# pybind_kv.set("test", "123", "/home/ubuntu/Desktop/incubator-resilientdb/scripts/deploy/config_out/client.config")
+# print(pybind_kv.get("a", "/home/ubuntu/Desktop/incubator-resilientdb/scripts/deploy/config_out/client.config"))
 
 
 
