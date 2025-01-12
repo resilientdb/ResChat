5c708aa2e0b3fd17e20c3aeeb83456acb47c8f6a
NoBugInMyCode <2940009621@qq.com>
2025-01-11 20:30:54 -0800

    update

--- backend/client.py
+++ backend/client.py
@@ -6,18 +6,20 @@ import Crypto
 from Crypto.PublicKey import RSA
 
 """Global Variables"""
+"""Following variables will be only initialize once after user login successfully """
 my_username: str
 my_public_key: Crypto.PublicKey.RSA.RsaKey
 my_private_key: Crypto.PublicKey.RSA.RsaKey
 my_friend_list: dict
 my_password: str
 my_public_key_string: str
-
+"""Following variables will be initialized and reinitialized everytime user choose a friend to talk with"""
 current_chatting_friend_username: str
 current_chatting_friend_public_key: Crypto.PublicKey.RSA.RsaKey
 current_chatting_page_name: str
 current_chatting_page_number: int
 current_chat_previous_page_number: int
 
+"""This variable will be initialized and reinitialized everytime user choose a friend to talk with and every two seconds"""
 current_chat_history: {}
 
